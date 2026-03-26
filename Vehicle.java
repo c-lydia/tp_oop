01e74ea99ae389a09ceb70ae7205f995c6553697
Lydiya Chheng <chhenglydiacl@gmail.com>
2026-03-26 15:46:18 +0700

    OOP

---
+++ Vehicle.java
@@ -0,0 +1,23 @@
+package oop.tp_oop.project_vehicle;
+
+public class Vehicle {
+    // Attrirbutes
+    String engine;
+    int speed;
+    int price;
+
+    // Constructor for Mr/Ms.AAA
+    Vehicle(int price) {
+    
+    }
+
+    // method 1 for Mr/Ms.BBB
+    void accelerate(int speed) {
+
+    }
+
+    // method 2 for Mr/Ms.CCC
+    void stop() {
+
+    }
+}
